0179c18a5bc335b13023285333f4717f8a733109
Phillip Nixon Hulleza <phillipnixon.hulleza@chmsu.edu.ph>
2026-05-30 14:56:42 +0800

    Update database credentials for production

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,9 @@
 <?php
 
 $host     = 'localhost';
-$db_name  = 'seed cycle';
-$username = 'root';
-$password = '';
+$db_name  = 'u500694472_seedcycle';
+$username = 'u500694472_seedCycle'; // Capital C for username
+$password = '$eedCycle1';
 
 $conn = mysqli_connect($host, $username, $password, $db_name);
 
